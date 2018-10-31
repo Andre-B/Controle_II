b069994c38b8c195275b3261f8c6359197b748dd
Andre-B <andrebicas.ab@gmail.com>
2018-10-31 14:42:46 -0200

    Primeiro commit

---
+++ ExercicioAvaliativo_3.m
@@ -0,0 +1,38 @@
+% Exercício avaliativo - Parte prática
+% Sistemas de Controle II
+% André Bicalho
+
+%% Parte 1
+
+Dz=1;
+K=1;
+Gs=tf(20,[1 6]);
+Hz=0.9;
+
+T=0.05;
+
+Gz=c2d(Gs,T,'zoh');
+
+
+
+
+a=1;
+b=30;
+
+while(abs(a-b)>1e-7)
+
+    K=(a+b)/2;
+    
+    SysD=feedback(Dz*K*Gz,Hz);
+
+    r=pole(SysD);
+    rm=max(abs(r));
+    if(rm>1)
+        b=K;
+    else
+        a=K;
+    end
+    
+end
+format long;
+disp(K)
